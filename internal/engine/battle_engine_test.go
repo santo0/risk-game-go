614640eb407a55b32f0082fce1ca417b32f3f6e7
santo0 <lemarti3472@gmail.com>
2023-09-30 04:01:57 +0200

    battle engine test

--- internal/engine/battle_engine_test.go
+++ internal/engine/battle_engine_test.go
@@ -1,15 +1,24 @@
 package engine_test
 
 import (
-	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/santo0/risk-game-go/internal/engine"
+	"github.com/santo0/risk-game-go/internal/model"
 
 	constants "github.com/santo0/risk-game-go/internal"
 )
 
 func TestBattleEngine(t *testing.T) {
 	btf := engine.CreateBattlefieldMap(constants.MAP_FILE, engine.DiceBattleConfiguration{})
-	fmt.Println(btf.GetPositions())
+	expectedPositions := map[int][]int{0: {2, 3, 4}, 1: {2}, 2: {0, 1}, 3: {0}, 4: {0}}
+	if !reflect.DeepEqual(btf.GetPositions(), expectedPositions) {
+		t.Fail()
+	}
+	army := model.Army{PlayerId: 1, Quantity: 10}
+	btf.SetPositionArmy(0, army)
+	if btf.GetPositionArmy(0) != army {
+		t.Fail()
+	}
 }
